143660fec07cc8a7d3b3573f6df69af03689fe9a
aadil42 <adilkelawala@gmail.com>
2023-10-25 16:36:27 +0530

    display 7 most recent expenses.

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -1,12 +1,21 @@
+import { useContext } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { GlobalStyles } from "../constants/styles";
 
+import List from "../components/List";
+
+import { ExpenseCtx } from "../store/context/ExpenseContext";
+
 const RecentExpenses = () => {
+
+    const ctx = useContext(ExpenseCtx);
+    const recentList = ctx.list.sort((a,b) => {
+        return b.date - a.date;        
+    });
+
     return (
         <View style={styles.container}>
-            <Text style={styles.text}>
-                This is recent
-            </Text>
+            <List list={recentList.slice(0, 7)} />
         </View>
     );
 }
