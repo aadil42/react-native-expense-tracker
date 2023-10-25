2fbc03cac64b244624ed968afb56f14a6cb0139f
aadil42 <adilkelawala@gmail.com>
2023-10-25 17:28:38 +0530

    add 7day total amount

--- screens/RecentExpenses.js
+++ screens/RecentExpenses.js
@@ -15,6 +15,10 @@ const RecentExpenses = () => {
 
     return (
         <View style={styles.container}>
+            <View style={styles.recentTotalContainer}>
+                <Text style={styles.lightTextColor}>Last 7 days</Text>
+                <Text style={styles.darkTextColor}>${recentList.slice(0, 7).reduce((acc, curr) => (acc+ +(curr.amount)), 0)}</Text>
+            </View>
             <List list={recentList.slice(0, 7)} />
         </View>
     );
@@ -27,9 +31,25 @@ const styles = StyleSheet.create({
         alignItems: "center",
         backgroundColor: GlobalStyles.colors.primary700
     },
-    text: {
-        color: "white",
-        fontSize: GlobalStyles.fonts.sizeL
+    lightTextColor: {
+        opacity: .7,
+        fontSize: 20,
+        color: GlobalStyles.colors.primary500
+    },
+    darkTextColor: {
+        fontSize: 25,
+        fontWeight: "bold",
+        color: GlobalStyles.colors.primary500
+    },
+    recentTotalContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "95%",
+        marginTop: 20,
+        backgroundColor: "white",
+        borderRadius: 5,
+        paddingHorizontal: 15,
+        paddingVertical: 8,
     }
 });
 
